79be07e3f7d2675eede7eee9e7e92850f7cc9e54
paulng99 <paulng02@hotmail.com>
2022-03-21 18:22:46 +0800

    2022-03-21

--- cccmmwc/src/features/App/AppRoute.tsx
+++ cccmmwc/src/features/App/AppRoute.tsx
@@ -3,10 +3,10 @@ import { Route } from "react-router";
 import { Dashboard } from "../Dashboard/Dashboard";
 import { IonRouterOutlet } from "@ionic/react";
 
-export const AppRoute = ()=>(
-    <IonRouterOutlet>
-    <Route path='/dashboard' component={Dashboard}/>
-    <Route exact path='/login' component={Dashboard}/>
-    <Route exact path='/logout' component={Dashboard}/>
+export const AppRoute = () => (
+  <IonRouterOutlet>
+    <Route path='/dashboard' component={Dashboard} />
+    <Route exact path='/login' component={Dashboard} />
+    <Route exact path='/logout' component={Dashboard} />
   </IonRouterOutlet>
 );
