288e9c1cfb167b414a40ec307ceb6a3ff120a470
paulng99 <paulng02@hotmail.com>
2022-03-28 00:20:06 +0800

    2022-03-28

--- cccmmwc/src/features/Test.tsx
+++ cccmmwc/src/features/Test.tsx
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { collection, collectionGroup, doc, getDoc, getDocs, query } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../services/firebase";
 
@@ -7,16 +7,20 @@ const Test = (prop: any) => {
     const [dataSnap, setDataSnap] = useState<any[]>([]);
 
     useEffect(() => {
-        const accessRef = doc(db, "modules", "Users");
-        getDoc(accessRef).then((d: any) => {
-            setDataSnap(d.data());
-            console.log(d.data())
+        const accessRef = query(collection(db, "modules"))
+        getDocs(accessRef).then((d: any) => {
+            d.forEach((d1: any) => {
+                console.log(d1.id)
+            });
+            setDataSnap(d);
+        }, (error) => {
+            console.log(error)
         });
     }, []);
 
     return (
         <>
-            {dataSnap.toLocaleString()}
+            {JSON.stringify(dataSnap)}
         </>
     );
 }
