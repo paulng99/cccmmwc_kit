328e552ad6dc49c1c653014d65f81e8d77b1c59a
paulng99 <paulng02@hotmail.com>
2022-03-31 17:25:19 +0800

    2022-03-31

--- cccmmwc/src/App.tsx
+++ cccmmwc/src/App.tsx
@@ -20,14 +20,17 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 import { AppRoute } from './features/App/AppRoute';
+import { AppContext } from './features/App/AppContext';
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <AppRoute />
-    </IonReactRouter>
+    <AppContext.Provider value={{}}>
+      <IonReactRouter>
+        <AppRoute />
+      </IonReactRouter>
+    </AppContext.Provider>
   </IonApp>
 );
 
--- cccmmwc/src/features/Test.tsx
+++ cccmmwc/src/features/Test.tsx
@@ -1,17 +1,18 @@
-import { collection, collectionGroup, doc, getDoc, getDocs, query, where } from "firebase/firestore";
+import { collection, collectionGroup, doc, DocumentData, DocumentReference, getDoc, getDocs, query, QueryDocumentSnapshot, QuerySnapshot, where } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "../services/firebase";
 
 const Test = (prop: any) => {
 
-    const [dataSnap, setDataSnap] = useState<any[]>([]);
+    const [dataSnap, setDataSnap] = useState<DocumentData>([]);
 
     useEffect(() => {
         const accessRef = query(collectionGroup(db, "menus"), where("accessGroups", "array-contains-any", ["RnM78h1Mdt4OzgB1DsbX", "mfUE8YkW2CQUy12emKgP"]))
-        getDocs(accessRef).then((d: any) => {
-            d.forEach((d1: any) => {
-                console.log(d1.id)
-                setDataSnap([...dataSnap, d1.data()]);
+        getDocs(accessRef).then((d: QuerySnapshot) => {
+            console.log(d.size)
+            d.forEach((d1: QueryDocumentSnapshot) => {
+                console.log(d1.ref.parent.parent?.id);
+                
             });
         }, (error) => {
             console.log(error)
@@ -20,7 +21,7 @@ const Test = (prop: any) => {
 
     return (
         <>
-        {JSON.stringify(dataSnap)}
+            
         </>
     );
 }
