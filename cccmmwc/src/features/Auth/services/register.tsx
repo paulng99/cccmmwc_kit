66db5329343ad4930c15949e95754fda2ceb7d68
paulng99 <paulng02@hotmail.com>
2022-04-04 13:32:04 +0800

    2022-04-04

--- cccmmwc/src/features/Auth/services/register.tsx
+++ cccmmwc/src/features/Auth/services/register.tsx
@@ -2,15 +2,15 @@ import { UserInfo } from "firebase/auth"
 import { collection, doc, getDoc, onSnapshot, query, where } from "firebase/firestore"
 import { db } from "../../../services/firebase"
 
-const register = async (userInfo: UserInfo) => {
+const useRegister = async (userInfo: UserInfo) => {
     if (userInfo.email) {
         const docRef = collection(db, "users");
         const groupsRef = query(docRef,where("groups","array-contains","teacher"))
         const unsub = onSnapshot(groupsRef, (doc) => {
             console.log(doc.docs);
-            return doc.docs;
+            return doc;
         });
     }
 }
 
-export { register }
+export { useRegister }
--- cccmmwc/src/features/Test.tsx
+++ cccmmwc/src/features/Test.tsx
@@ -1,43 +1,7 @@
-import { IonButton } from "@ionic/react";
-import { collectionGroup, DocumentData, getDocs, query, QueryDocumentSnapshot, QuerySnapshot, where } from "firebase/firestore";
-import { useContext, useEffect, useState } from "react";
-import { db } from "../services/firebase";
-import { AppContext } from "./App/AppContext";
-import { register } from "./Auth/services/register";
 
 const Test = (prop: any) => {
-
-    const { appState, appDispatch } = useContext(AppContext);
-    const [dataSnap, setDataSnap] = useState<DocumentData>([]);
-    const [vars, setVars]= useState(null);
-
-    useEffect(() => {
-        const accessRef = query(collectionGroup(db, "menus"), where("accessGroups", "array-contains-any", ["RnM78h1Mdt4OzgB1DsbX", "mfUE8YkW2CQUy12emKgP"]))
-        getDocs(accessRef).then((d: QuerySnapshot) => {
-            console.log(d.size)
-            d.forEach((d1: QueryDocumentSnapshot) => {
-                console.log(d1.ref.path);
-
-            });
-        }, (error) => {
-            console.log(error)
-        });
-        register(appState.userInfo).then((r)=>{
-            setVars(r)
-        })
-    }, []);
-
-    const handleClick = () => {
-        appDispatch({
-            "type": "LOADING",
-            "payload": !appState.isLoading,
-        })
-    }
-
     return (
         <>
-            {vars}<br></br>
-            <IonButton onClick={handleClick}>Change Loading</IonButton>
         </>
     );
 }
