13e0f4522d62bbf5beced721cd789b600fa96141
paulng99 <paulng02@hotmail.com>
2022-04-09 12:33:54 +0800

    2022-04-09

---
+++ cccmmwc/src/configs/firebaseConfig.tsx
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAtFkx3MgdSx_GttG2qeyCe5__WZxVLwV4",
+    authDomain: "cccmmwc-cfeec.firebaseapp.com",
+    projectId: "cccmmwc-cfeec",
+    storageBucket: "cccmmwc-cfeec.appspot.com",
+    messagingSenderId: "554697695134",
+    appId: "1:554697695134:web:99705afab367739ba3c08f",
+    measurementId: "G-RJQ61GJDVS"
+  };
--- cccmmwc/src/services/firebase.tsx
+++ cccmmwc/src/services/firebase.tsx
@@ -2,21 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getRemoteConfig } from "firebase/remote-config";
+import { firebaseConfig } from "../configs/firebaseConfig";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAtFkx3MgdSx_GttG2qeyCe5__WZxVLwV4",
-  authDomain: "cccmmwc-cfeec.firebaseapp.com",
-  projectId: "cccmmwc-cfeec",
-  storageBucket: "cccmmwc-cfeec.appspot.com",
-  messagingSenderId: "554697695134",
-  appId: "1:554697695134:web:99705afab367739ba3c08f",
-  measurementId: "G-RJQ61GJDVS"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
