d2540df9ae7ece582f35bb700386640ce5c598a4
paulng99 <paulng02@hotmail.com>
2022-04-13 20:57:44 +0800

    2022-04-13

--- cccmmwc_antd/src/index.tsx
+++ cccmmwc_antd/src/index.tsx
@@ -1,21 +1,33 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
+
+
+ ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+); */
+
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+import { createRoot } from 'react-dom/client';
+
+
+const container = document.getElementById('root');
+const root = createRoot(container as Element);
+root.render(<App />);
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
