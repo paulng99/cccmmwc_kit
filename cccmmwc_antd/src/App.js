c9eb8a655512a7d89e47b24241b4e5d6e638f7fc
paulng99 <paulng02@hotmail.com>
2022-04-13 17:00:15 +0800

    2022-04-14

--- cccmmwc_antd/src/App.js
+++ cccmmwc_antd/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import { Button } from 'antd';
-
+import axios from "axios"
 
 function App() {
+
+  const handleClick = () => {
+    axios.get("http://www.yahoo.com").then((r)=>{
+      console.log(r);
+    });
+
+  }
+
   return (
-    <Button shape='circle' size='large'>Hello</Button>
+    <Button shape='circle' size='large' onClick={handleClick}>Hello</Button>
   );
 }
 
