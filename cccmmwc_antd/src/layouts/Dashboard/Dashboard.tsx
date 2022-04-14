3237a8cd5d7316d8aaaf01d845bcb08344779832
paulng99 <paulng02@hotmail.com>
2022-04-14 13:29:46 +0800

    2022-04-14

--- cccmmwc_antd/src/layouts/Dashboard/Dashboard.tsx
+++ cccmmwc_antd/src/layouts/Dashboard/Dashboard.tsx
@@ -1,13 +1,19 @@
+import { Layout } from "antd";
 import { FC } from "react";
 import "./Dashboard.css"
 
+const { Sider, Header, Content } = Layout;
 
 const Dashboard: FC = (prop: any) => {
-    
-    return (
-        <>
 
-        </>
+    return (
+        <Layout>
+            <Sider>Left menu</Sider>
+            <Content>
+                <Header>Header</Header>
+                {prop.children}</Content>
+            <Sider>Right menu</Sider>
+        </Layout>
     );
 }
 
--- cccmmwc_antd/src/modules/App/App.tsx
+++ cccmmwc_antd/src/modules/App/App.tsx
@@ -1,8 +1,13 @@
+import { Route, Routes } from 'react-router';
+import { Test } from '../Test/Test';
 import './App.css';
 
 function App() {
   return (
     <>
+    <Routes>
+      <Route path='/test' element={<Test />} />
+    </Routes>
     </>
   );
 }
