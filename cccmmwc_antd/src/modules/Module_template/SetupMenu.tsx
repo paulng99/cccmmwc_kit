6935ebb85d38f4ac96235f6fa68b44aa02f6654e
paulng99 <paulng02@hotmail.com>
2022-04-15 22:27:05 +0800

    2022-04-15

---
+++ cccmmwc_antd/src/modules/Module_template/SetupMenu.tsx
@@ -0,0 +1,24 @@
+export const menus={
+    "name_en": "",
+    "canReadByUsers": [],
+    "menus": {
+        "sub_menu": [
+            {
+                "name_en": "Sub menu 1",
+                "name_zh": "副選單1"
+            },
+            {
+                "name_en": "Sub Menu 2",
+                "name_zh": "副選單 2"
+            }
+        ],
+        "name_en": "Main Menu 1",
+        "name_zh": "主選單 1"
+    },
+    "canWriteByUsers": [
+        "mmw-nty@cccmmwc.edu.hk"
+    ],
+    "name_zh": "",
+    "canReadByGroups": [],
+    "canWriteByGroups": []
+}
--- cccmmwc_antd/src/modules/Test/Test.tsx
+++ cccmmwc_antd/src/modules/Test/Test.tsx
@@ -1,23 +1,20 @@
-import { collectionGroup, DocumentData, getDocs, QuerySnapshot } from "firebase/firestore";
+import { doc, DocumentData, DocumentSnapshot, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react"
 import Dashboard from "../../layouts/Dashboard/Dashboard"
 import { db } from "../../services/firebase";
 
 export const Test = () => {
-    const [data, setData] = useState<DocumentData[]>();
+    const [data, setData] = useState<DocumentData>();
 
     useEffect(
-        ()=>{
-            const dataArray: DocumentData[]=[];
-            const menuQuery=collectionGroup(db,"menus");
-            getDocs(menuQuery).then(d=>{
-                d.forEach(dd=>{
-                    dataArray.push(dd.data());
-                })
-                console.log(dataArray)
-                setData(dataArray);
-            })
-        },[]);
+        () => {
+            const menuQuery = doc(db, "modules", "Grouping", "rights", "add_grouping");
+            getDoc(menuQuery).then((d:DocumentSnapshot) => {
+                console.log(d.data());
+                setData(d.data());
+             }
+            )
+        }, []);
 
     return (
         <Dashboard>
@@ -25,3 +22,4 @@ export const Test = () => {
         </Dashboard>
     )
 }
+
