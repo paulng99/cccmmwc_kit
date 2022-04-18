110fd285ebc6ef9329022e3b4b7b5508a79fc516
paulng99 <paulng02@hotmail.com>
2022-04-18 22:12:16 +0800

    2022-04-18

--- cccmmwc_antd/src/modules/App/AppContext.tsx
+++ cccmmwc_antd/src/modules/App/AppContext.tsx
@@ -1,10 +1,15 @@
 import { createContext, FC } from "react";
 
-export const AppContext = createContext({});
+export const initialAppState ={
+    userInfo:{},
+    loading:false
+}
+
+export const AppContext = createContext(initialAppState);
 
 export const AppProvider:FC =({children})=>{
     return (
-        <AppContext.Provider value={appStat, appDispatch}>
+        <AppContext.Provider value={appState, appDispatch}>
         {children}
         </AppContext.Provider>
     );
