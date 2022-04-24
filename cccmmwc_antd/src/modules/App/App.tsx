2606a75a6555a2315d64c2f02b757612c08e6a8a
paulng99 <paulng02@hotmail.com>
2022-04-24 21:39:24 +0800

    2022-04-24

--- cccmmwc_antd/src/modules/App/App.tsx
+++ cccmmwc_antd/src/modules/App/App.tsx
@@ -1,8 +1,5 @@
 import { Route, Routes } from 'react-router';
 import { AuthHome } from '../Auth/Home';
-import Login from '../Auth/Login';
-import Logout from '../Auth/Logout';
-import { AuthSetup } from '../Auth/Setup';
 import Setup from '../Module_template/Setup/Setup';
 import { Test } from '../Test/Test';
 import './App.css';
