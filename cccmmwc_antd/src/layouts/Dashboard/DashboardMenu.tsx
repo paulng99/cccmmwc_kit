71ab48aa2e51f9a559ec7cbdcf6a12e4609619a7
paulng99 <paulng02@hotmail.com>
2022-04-24 22:30:27 +0800

    2022-04-24

--- cccmmwc_antd/src/layouts/Dashboard/DashboardMenu.tsx
+++ cccmmwc_antd/src/layouts/Dashboard/DashboardMenu.tsx
@@ -2,32 +2,33 @@ import React, { useEffect, useState } from "react"
 import { Menu } from "antd";
 import useMenus from "../../modules/App/hooks/useMenus";
 import { ItemType } from "antd/lib/menu/hooks/useItems";
-import {UserOutlined} from "@ant-design/icons"
+import { UserOutlined } from "@ant-design/icons"
+import { Link } from "react-router-dom";
 
 
 export default () => {
     const menus: [] = useMenus();
     const [menuData, setMenuData] = useState<ItemType[]>();
-    let m:any[]= [];
+    let m: any[] = [];
 
     useEffect(() => {
-        let menuLocal=JSON.parse(localStorage.getItem("menus")!)
-        menuLocal.forEach((ml:any) => {
+        let menuLocal = JSON.parse(localStorage.getItem("menus")!)
+        menuLocal.forEach((ml: any) => {
             m.push({
-                "key":Math.random()*100,
-                "label":ml.name_en,
-                "link":ml.menu.link,
-                "icon":React.createElement(ml.icon||UserOutlined),
-                "children":[{
-                    "label":ml.menu.name_en,
-                    "key":Math.random()*100,
+                "key": Math.random() * 100,
+                //"label":<Link to="">{ml.name_zh}</Link>,
+                "label": React.createElement(Link,{"to":ml.menu.link},ml.name_en),
+                "icon": React.createElement(ml.icon || UserOutlined),
+                "children": [{
+                    "label": ml.menu.name_en,
+                    "key": Math.random() * 100,
                 }],
             })
         });
         setMenuData(m)
     }, [menus]);
 
-    
+
 
     return (
         <>
