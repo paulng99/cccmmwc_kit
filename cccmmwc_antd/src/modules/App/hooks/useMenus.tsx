d08402c74c6fef74e9011916d46e84d0867e7f22
paulng99 <paulng02@hotmail.com>
2022-04-27 17:31:06 +0800

    2022-04-27

--- cccmmwc_antd/src/modules/App/hooks/useMenus.tsx
+++ cccmmwc_antd/src/modules/App/hooks/useMenus.tsx
@@ -4,13 +4,17 @@ import { AppContext } from "../AppContext";
 import { useGetAccess } from "./useAccess";
 
 export default () => {
-    const {appState, appDispatch} = useContext(AppContext);
+    const { appState, appDispatch } = useContext(AppContext);
     const [menus, setMenus] = useState<any>([]);
-    const access = useGetAccess(appState.userInfo.email);
+    let access: any[] = [];
 
-
+    if (!localStorage.getItem("access")) {
+        access = useGetAccess(appState.userInfo.email);
+    }
+    
     useEffect(() => {
-        setMenus(_.groupBy(access, x=>x.module_id))
+
+        setMenus(_.groupBy(access, x => x.module_id))
     }, [access])
     console.log(menus)
     return menus;
