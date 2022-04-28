77c8d13f07a391b29fd8528939bd4986fbc0461f
paulng99 <paulng02@hotmail.com>
2022-04-28 08:15:13 +0800

    20220428

--- cccmmwc_antd/src/modules/Test/Test.tsx
+++ cccmmwc_antd/src/modules/Test/Test.tsx
@@ -1,16 +1,13 @@
 import { useContext, useEffect, useState } from "react"
 import Dashboard from "../../layouts/Dashboard/Dashboard";
 import { AppContext } from "../App/AppContext";
-import useMenus from "../App/hooks/useMenus";
 
 export const Test = () => {
     const { appState, appDispatch } = useContext(AppContext)
     const [data, setData] = useState({});
-    const menus = useMenus();
 
     useEffect(() => {
-        setData(menus);
-    }, [appState, menus]);
+    }, [appState]);
 
     return (
         <Dashboard>
