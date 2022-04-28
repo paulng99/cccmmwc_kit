6631ac3e9cad51378795b7738db9e90dee11abc6
paulng99 <paulng02@hotmail.com>
2022-04-28 08:14:57 +0800

    20220428

--- cccmmwc_antd/src/modules/App/hooks/useMenus.tsx
+++ cccmmwc_antd/src/modules/App/hooks/useMenus.tsx
@@ -1,20 +1,37 @@
+import { doc, getDoc } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
-import _ from "underscore";
+import _q from "underscore";
+import { db } from "../../../services/firebase";
 import { AppContext } from "../AppContext";
 import { useGetAccess } from "./useAccess";
 
 export default () => {
     const { appState, appDispatch } = useContext(AppContext);
     const [menus, setMenus] = useState<any>([]);
-    let access: any[] = [];
+    const [tempMenus, setTempMenus] = useState({});
+    const access = useGetAccess(appState.userInfo.email);
 
-    if (!localStorage.getItem("access")) {
-        access = useGetAccess(appState.userInfo.email);
+
+    const convertToMenus = () => {
+        let m: { label: any; link: any; children: unknown; }[] = [];
+        for (const [key, values] of Object.entries(tempMenus)) {
+            console.log(tempMenus)
+            getDoc(doc(db, 'modules', key)).then(o => {
+                let d: any = o.data();
+                m.push({
+                    "label": d.menus.name_en,
+                    "link": d.menus.link,
+                    "children": values
+                })
+                console.log(m)
+            })
+        }
+        setMenus(m)
     }
-    
-    useEffect(() => {
 
-        setMenus(_.groupBy(access, x => x.module_id))
+    useEffect(() => {
+        setTempMenus(_.groupBy(access, x => x.module_id))
+        convertToMenus();
     }, [access])
     console.log(menus)
     return menus;
