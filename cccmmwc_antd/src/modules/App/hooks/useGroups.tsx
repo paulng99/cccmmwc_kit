4efb0eabab3c680d0b4d0502b46b1c9ea636d8ab
paulng99 <paulng02@hotmail.com>
2022-04-30 22:45:38 +0800

    2022-04-30

--- cccmmwc_antd/src/modules/App/hooks/useGroups.tsx
+++ cccmmwc_antd/src/modules/App/hooks/useGroups.tsx
@@ -2,13 +2,10 @@ import { doc, getDoc } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react"
 import { db } from "../../../services/firebase";
 import { decryptDataToString } from "../../../utils/encrypto";
-export default (e: any | null = null) => {
-    const [email, setEmail] = useState<string>(e)
+export default (user: any | null = null) => {
+    const [userInfo, setUserInfo] = useState<any>(user||JSON.parse(decryptDataToString(localStorage.getItem("userInfo"))))
     const [groups, setGroups] = useState(["guest"])
-    let userInfo = { "email": "" }
-    if (localStorage.getItem("userInfo")) {
-        userInfo = JSON.parse(decryptDataToString(localStorage.getItem("userInfo")));
-    }
+
 
     useEffect(() => {
         userInfo.email && getDoc(doc(db, "users", userInfo.email)).then(d => {
@@ -16,7 +13,7 @@ export default (e: any | null = null) => {
             setGroups(user?.groups)
             console.log(groups)
         })
-    }, [email])
+    }, [userInfo])
     localStorage.setItem("groups", JSON.stringify(groups))
     return groups
 }
