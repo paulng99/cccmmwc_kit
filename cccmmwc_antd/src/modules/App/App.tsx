35c79ebd251cbd737d56887bedb2f79a5a721413
paulng99 <paulng02@hotmail.com>
2022-05-10 00:23:34 +0800

    2022-05-09

--- cccmmwc_antd/src/modules/App/App.tsx
+++ cccmmwc_antd/src/modules/App/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router';
+import { Route, Routes, useLocation } from 'react-router';
 import _ from 'underscore';
 import { AuthHome } from '../Auth/Home';
 import Setup from '../Module_template/Setup/Setup';
@@ -6,8 +6,24 @@ import { AppSetup } from '../App/Setup';
 import { Test } from '../Test/Test';
 import './App.css';
 import { AppProvider } from './AppContext';
+import { useEffect } from 'react';
+import useGroups from './hooks/useGroups';
+import { decryptDataToString } from '../../utils/encrypto';
+import { useGetAccess } from './hooks/useAccess';
 
 function App() {
+  const { groups, setEmail } = useGroups();
+ // const getAccess = useGetAccess();
+  const { pathname } = useLocation();
+  useEffect(() => {
+    if (localStorage.getItem("userInfo")) {
+      let email = JSON.parse(decryptDataToString(localStorage.getItem("userInfo"))).email
+      setEmail(email)
+   }else{
+      setEmail(null)
+    }
+  }, [pathname])
+
   return (
     <AppProvider>
       <Routes>
--- cccmmwc_antd/src/modules/App/hooks/useGroups.tsx
+++ cccmmwc_antd/src/modules/App/hooks/useGroups.tsx
@@ -1,21 +1,21 @@
-import { doc, getDoc } from "firebase/firestore";
 import { useEffect, useState } from "react"
+import { doc, getDoc } from "firebase/firestore";
 import { db } from "../../../services/firebase";
 import { decryptDataToString } from "../../../utils/encrypto";
 
 export default (e: string | null = null) => {
-    const [groups, setGroups] = useState(["guest"])
-    const [email, setEmail] = useState<string|null>()
-
-
+    const [groups, setGroups] = useState<any[]>([])
+    const [email, setEmail] = useState<string | null>(e)
     useEffect(() => {
+        if (!localStorage.getItem("userInfo")){
+            setGroups(["guest"])
+        }
         email && getDoc(doc(db, "users", email)).then(d => {
             console.log("using firestore in groups: ", d.data())
             let user = d.data();
             setGroups(user?.groups)
-            localStorage.setItem("groups",JSON.stringify(groups))
         })
     }, [email])
-
+    localStorage.setItem("groups", JSON.stringify(groups))
     return { groups, setEmail }
 }
