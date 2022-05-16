6eb3f221b1d86bf5083a7bb0eb599607d3fc3cf5
paulng99 <paulng02@hotmail.com>
2022-05-17 00:33:55 +0800

    2022-05-16

--- cccmmwc_antd/src/modules/Grouping/UserGrouping.tsx
+++ cccmmwc_antd/src/modules/Grouping/UserGrouping.tsx
@@ -1,7 +1,38 @@
+import { PageHeader } from "antd"
+import { UserInfo } from "firebase/auth"
+import { collectionGroup, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react"
 import Dashboard from "../../layouts/Dashboard/Dashboard"
+import { db } from "../../services/firebase"
+import { decryptDataToString } from "../../utils/encrypto"
 
-export default ()=>{
+export default () => {
+    const [userInfo, setUserInfo] = useState<UserInfo>()
+    const [functions, setFunctions] = useState();
+    useEffect(() => {
+        let f:any[]=[];
+        setUserInfo(localStorage.getItem("userInfo") && JSON.parse(decryptDataToString(localStorage.getItem("userInfo"))));
+
+        getDocs(collectionGroup(db,"functions")).then((d)=>{
+            d.forEach(q=>{
+               f.push(q.data())
+            })
+            setFunctions(f);
+        })
+
+    }, [])
+
+    const FunctionsRender=()=>{
+        return (<>function list</>)
+    }
+
     return (<Dashboard>
-        user grouping   
+        {console.log(userInfo)}
+        <PageHeader
+            title={userInfo?.displayName}
+            subTitle={userInfo?.email}
+            avatar={{src:userInfo?.photoURL,size:"large"}}
+            footer={<FunctionsRender />}
+        />
     </Dashboard>)
 }
